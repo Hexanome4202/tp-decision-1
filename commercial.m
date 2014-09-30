229c16da28bdf6ccc4acebcd4e392e8fcad6d05f
Rémi Martin <mrmi11@yahoo.fr>
2014-09-30 10:12:49 +0200

    Added stocks and commercial functions
    Added stocks and commercial functions

---
+++ commercial.m
@@ -0,0 +1,8 @@
+function [ f ] = commercial( )
+%COMMERCIAL Summary of this function goes here
+%   Detailed explanation goes here
+
+f = [1; 1; 1; -1; -1; -1];
+
+end
+
--- stocks.m
+++ stocks.m
@@ -1,7 +1,8 @@
-function [ output_args ] = sotcks( input_args )
+function [ f ] = stocks( )
 %SOTCKS Summary of this function goes here
 %   Detailed explanation goes here
 
+f = [5; 5; 6; 10; 5; 4];
 
 end
 
