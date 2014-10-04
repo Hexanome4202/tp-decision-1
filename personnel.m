b2d9b8b26e38b81be1df65625f2a1f1597a5b196
Rémi Martin <mrmi11@yahoo.fr>
2014-10-04 17:06:27 +0200

    Updated personnel
    Alex did it ! \o/

--- personnel.m
+++ personnel.m
@@ -4,5 +4,13 @@ function [ f ] = personnel( )
 
 f = [13; 1; 11; 7; 20; 50];
 
+[Aperso, bperso] = constraints();
+
+Aperso(11,:) = -1*getOpti(comptable(), zeros(6,1), []);
+
+bperso(11) = 0.8*sum(getBenef(Aperso(11,:)));
+
+linprog(f, Aperso, bperso, [], [], zeros(6,1), []);
+
 end
 
