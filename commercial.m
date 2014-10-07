45019e84a5c2c682ad7a0207adcf688774cf8c18
titooan <titouan.thibaud@gmail.com>
2014-10-07 09:01:11 +0200

    Corrections des calculs pour le responsable Commercial

--- commercial.m
+++ commercial.m
@@ -2,10 +2,39 @@ function [ X ] = commercial( )
 %COMMERCIAL Summary of this function goes here
 %   Detailed explanation goes here
 
-f = [1; 1; 1; -1; -1; -1];
 
-[A,b]=constraints();
+[ A, b ] = constraints();
+
+A = [A;  1 1 1 -1 -1 -1; -1 -1 -1 1 1 1];
+
+lb = [0; 0; 0; 0; 0; 0];
+
+epsilon = [];
+
+benefs = [];
+
+
+
+f=-function_of_comptable;
     
-X = linprog(f, A, b, [], [], zeros(6,1), []);
+for epsi = 0:600
+    b1 = [b; epsi; epsi];
+    x = linprog(f, A, b1, [], [], lb)
+
+    benefs = [benefs sum(getBenef(x))];
+    abscisse = abs(x(1)+x(2)+x(3)-x(4)-x(5)-x(6));
+    epsilon = [epsilon epsi];
+end
+
+plot(epsilon, benefs);
+
+benefs
+
+
+%Nombre de produits pour le meilleur epsilon :
+b1 = [b; epsi; epsi];
+x = linprog(f, A, b1, [], [], lb);
+
+sum(x)
 
 end
