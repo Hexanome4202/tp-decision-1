adf9d3ce85d70c5f33454739860fdd111ae41dcd
Cécilia Van Bever <ceciliavanbever@gmail.com>
2014-10-07 09:26:03 +0200

    Added matriceDeGain
    Added matriceDeGain

---
+++ matriceDeGain.m
@@ -0,0 +1,51 @@
+function [  ] = matriceDeGain( )
+  %%Profit
+    %Comptable
+       produitsComptable = comptable;
+       pComptable = getBenef(produitsComptable);
+      
+    %Responsable d'Atelier
+       produitsAtelier = responsableDAtelier;
+       pAtelier = getBenef(produitsAtelier);
+       
+    %Responsable des stocks
+       produitsStocks = stocks;
+       pStocks = getBenef(produitsStocks);
+       
+    %Responsable commercial
+       produitsCommercial = commercial;
+       pCommercial = getBenef(produitsCommercial);
+       
+    %Responsable du personnel
+       produitsPersonnel = personnel;
+       pPersonnel = getBenef(produitsPersonnel);
+    
+  %%Products quantity
+    %Comptable
+    %Responsable d'Atelier
+    %Responsable des stocks
+    %Responsable commercial
+    %Responsable du personnel
+    
+  %% Stock quantity
+    %Comptable
+    %Responsable d'Atelier
+    %Responsable des stocks
+    %Responsable commercial
+    %Responsable du personnel
+    
+  %%  between 2 families
+    %Comptable
+    %Responsable d'Atelier
+    %Responsable des stocks
+    %Responsable commercial
+    %Responsable du personnel
+    
+  %%Use time 3&5
+    %Comptable
+    %Responsable d'Atelier
+    %Responsable des stocks
+    %Responsable commercial
+    %Responsable du personnel
+end
+
